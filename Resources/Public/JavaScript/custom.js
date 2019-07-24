db83c87987a439070446ce2a6407ceea9f79475f
Kallol Chakraborty <kallol@creativekallol.com>
2019-07-24 16:27:46 +0530

    [BUGFIX] Object doesn't support on IE 11

--- Resources/Public/JavaScript/custom.js
+++ Resources/Public/JavaScript/custom.js
@@ -2,7 +2,13 @@
     $(document).ready(function () {
         // Using setTimeout to run after other ready callbacks
         setTimeout(function () {
-            window.dispatchEvent(new Event('resize'));
+            if (navigator.userAgent.indexOf('MSIE') !== -1 || navigator.appVersion.indexOf('Trident/') > 0) {
+                var evt = document.createEvent('UIEvents');
+                evt.initUIEvent('resize', true, false, window, 0);
+                window.dispatchEvent(evt);
+            } else {
+                window.dispatchEvent(new Event('resize'));
+            }
         }, 500);
     });
 })(jQuery);
