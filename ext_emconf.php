459c9d1189b9a1b0ee2c0d32c18019b21b42f67f
Sagar Desai <sagardesai1990@gmail.com>
2021-05-03 22:05:53 +0200

    [TASK] Update version and dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,17 +1,18 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['lms3h5p'] = [
     'title' => 'LMS3 H5P',
     'description' => 'H5P Platform Integration Plugin for TYPO3 CMS.',
     'category' => 'misc',
     'author' => 'Sagar Desai',
-    'author_email' => 'sagar.desai@lms3.de',
+    'author_email' => 'sagardesai1990@gmail.com',
+    'author_company' => 'LEARNTUBE! GmbH',
     'state' => 'beta',
     'clearCacheOnLoad' => true,
     'version' => '1.2.6',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-10.5.99',
+            'typo3' => '10.0.0-10.5.99',
         ],
     ],
 ];
