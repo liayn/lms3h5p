dd3acab9a3f0c37aaf80cd61ff48d420152e298d
Sagar Desai <sagardesai1990@gmail.com>
2022-10-15 20:22:16 +0200

    [TASK] Update config file

---
+++ ext_emconf.php
@@ -0,0 +1,18 @@
+<?php
+
+$EM_CONF['lms3h5p'] = [
+    'title' => 'LMS3 H5P',
+    'description' => 'H5P Platform Integration Plugin for TYPO3 CMS.',
+    'category' => 'misc',
+    'author' => 'Sagar Desai',
+    'author_email' => 'sagardesai1990@gmail.com',
+    'author_company' => 'LEARNTUBE! GmbH',
+    'state' => 'beta',
+    'clearCacheOnLoad' => true,
+    'version' => '2.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.0.0-11.5.99',
+        ],
+    ],
+];
