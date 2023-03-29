e79425e2407b85be62f60a6161d4c411e030ecd6
Serhii Borulko <borulkosergey@icloud.com>
2023-03-29 15:59:13 +0300

    [BUGFIX] Replace datatables by tablesort

--- Resources/Public/JavaScript/DataTable.js
+++ Resources/Public/JavaScript/DataTable.js
@@ -1,22 +1,17 @@
-define(['jquery', 'datatables'], function($) {
+define(['tablesort'], function($) {
     var Module = {};
 
     Module.initializeDataTables = function() {
-        $('#h5p-content').DataTable({
-            "columnDefs": [
-                { "orderable": false, "targets": 6 }
-            ],
-            "lengthMenu": [[10, 25, 50, 100, 200, 500], [10, 25, 50, 100, 200, 500]]
-        });
-        $('#h5p-library').DataTable({
-            "columnDefs": [
-                { "orderable": false, "targets": 5 }
-            ],
-            "lengthMenu": [[10, 25, 50, 100, 200, 500], [10, 25, 50, 100, 200, 500]]
-        });
+        new Tablesort(
+            document.getElementById('h5p-content')
+        );
+
+        new Tablesort(
+            document.getElementById('h5p-library')
+        );
     };
 
-    $(document).ready(function() {
+    document.addEventListener("DOMContentLoaded",function() {
         Module.initializeDataTables();
     });
 });
